193c94fa415dc030bfd8fba2b90c5cac9de312f7
Yura Husak <yura.husak@juriba.com>
2021-07-10 23:22:42 +0300

    Format project's files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,16 @@ import { FormlyModule } from '@ngx-formly/core';
 import { FormlyMaterialModule } from '@ngx-formly/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    FormlyModule.forRoot({ extras: { lazyRender: true } }),
-    FormlyMaterialModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		ReactiveFormsModule,
+		FormlyModule.forRoot({ extras: { lazyRender: true } }),
+		FormlyMaterialModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
